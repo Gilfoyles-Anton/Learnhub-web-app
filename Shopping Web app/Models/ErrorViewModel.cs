b2f527a63de9039df839c670b942ec808c1c42a7
Gilfoyles-Anton <1921441793@qq.com>
2025-05-20 19:36:47 +0800

    添加项目文件。

---
+++ Shopping Web app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Shopping_Web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
